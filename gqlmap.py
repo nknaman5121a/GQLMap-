508a95f46eec06d3ee90c0b0a08b2434a59e6611
Naman kumawat <115345375+nknaman5121a@users.noreply.github.com>
2025-07-08 23:22:48 +0530

    Update gqlmap.py

--- gqlmap.py
+++ gqlmap.py
@@ -110,19 +110,24 @@ def main():
        #print(f"Generated On:       {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
         # Only save report if output flag is given
-    if args.output:
-        domain = urlparse(base_url).netloc.replace('.', '_')
-        report_path = args.report or f"output/{domain}_report.{args.output}"
-        generate_report(
-            data=report_data,
-            output_file=report_path,
-            output_format=args.output
-        )
-
-    # Step 6: Auth Bypass
-    if args.token or args.test_roles or args.header_fuzz:
-        test_auth_bypass(base_url, target_endpoint, args)
-
+       if args.output:
+            domain = urlparse(base_url).netloc.replace('.', '_')
+            report_path = args.report or f"output/{domain}_report.{args.output}"
+    
+            report_data = {
+                "url": base_url,
+                "endpoint": target_endpoint or "N/A",
+                "introspected": bool(args.introspect),
+                "injection_tested": bool(args.inject),
+                "mutation_tested": bool(args.mutate)
+            }
+    
+            generate_report(
+                data=report_data,
+                output_file=report_path,
+                output_format=args.output
+            )
+            print(f"[+] Report saved to {report_path}")
 
 if __name__ == "__main__":
     main()
